ebd1fae9ef7fafc05d393453f756b7e0605591eb
Vaibhav_Pratap_Singh <vaibhavpsingh179@gmail.com>
2024-10-29 02:07:12 +0530

    Update for comment sectionjs.js

--- js.js
+++ js.js
@@ -20,3 +20,25 @@ document.addEventListener('scroll', function() {
       }
     });
   });
+ // Get comments
+fetch('https://portfoliobackend-k2gr.onrender.com')
+.then(response => response.json())
+.then(data => {
+    data.forEach(comment => {
+        // Display each comment on the webpage
+        console.log(comment.content);
+    });
+});
+
+// Post a comment
+fetch('https://portfoliobackend-k2gr.onrender.com', {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({ content: 'Your comment here' })
+})
+.then(response => response.json())
+.then(data => {
+console.log('Comment saved:', data);
+});
+
+
